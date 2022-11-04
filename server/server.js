857bfc29eea523cd37a912f1dc9e5889cbdd1030
Joon Park <jsp220@gmail.com>
2022-11-04 14:53:31 -0700

    add schemas

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
