797ddffa51ad2a210730db6d265188d6323ce38c
Joon Park <jsp220@gmail.com>
2022-11-05 00:00:57 -0700

    Add graphql routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,14 @@
-const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
+        books: async () => {
+            return Book.find();
+        },
+        user: async () => {
+
+        }
     }
 }
